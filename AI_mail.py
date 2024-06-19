5df2936f9eac17adac9a7036c950cb84436c748e
lordpba <mario.pba@gmail.com>
2024-06-19 11:02:01 +0200

    Update AI_mail.py to use ChatGroq model for language chaining

--- AI_mail.py
+++ AI_mail.py
@@ -3,24 +3,14 @@ import email
 from email.mime.text import MIMEText
 import smtplib
 import time
-from langchain_community.chat_models import ChatOllama
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-import pandas as pd
-import openpyxl
-import os
-from langchain_community.embeddings.openai import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
-from langchain_community.chat_models import ChatOpenAI
 from langchain_groq import ChatGroq
-from langchain.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
-from langchain_chroma import Chroma
-from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
-#from langchain_openai import ChatOpenAI
-import datetime
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import os
 from dotenv import load_dotenv
 from langsmith import traceable
+#from langchain_openai import ChatOpenAI
+#from langchain_ollama import ChatOllama
 
 # Load environment variables from .env file
 load_dotenv()
@@ -37,9 +27,7 @@ email_password = os.getenv('EMAIL_PASSWORD')  # Get password from environment va
 groq = ChatGroq(temperature=0.0, model_name="llama3-8b-8192") # mixtral-8x7b-32768 - llama3-70b-8192 - gemma-7b-it - llama3-8b-8192
 llm = groq
 
-
-
-# controlla mail non lette e returna email_message
+# Check for unread emails and return email_message
 def check_for_new_emails(email_account):
     email_message = None
     mail = imaplib.IMAP4_SSL(imap_host)
@@ -51,15 +39,15 @@ def check_for_new_emails(email_account):
             status, data = mail.fetch(num, '(RFC822)')
             if status == 'OK':
                 email_message = email.message_from_bytes(data[0][1])
-                # Estrai il contenuto dell'email qui e passalo al LLM
+                # Extract the email content here and pass it to the LLM
                 # process_email(email_message)
-                # generate_response_using_chroma(email_mesclearsage, vectordb)
+                # generate_response_using_chroma(email_message, vectordb)
                 # update_vector_store_and_reply(email_message)
     mail.close()
     mail.logout()
     return(email_message)
 
-# dalla mail estrae il corpo e returna body
+# Extract the body from the email and return body
 def get_email_body(email_message):
     body = ""
     if email_message is not None and email_message.is_multipart():
@@ -74,29 +62,29 @@ def get_email_body(email_message):
     # not multipart - i.e. plain text, no attachments, keeping fingers crossed
     else:
         body = email_message.get_payload(decode=True)
-        print("Corpo dell'email: ", body)
+        print("Email body: ", body)
 
     return body.decode('utf-8', errors='ignore')
 
-# processa il corpo dell'email e returna llm_response  
+# Process the email body and return llm_response  
 @traceable # Auto-trace this function  
 def process_email(body):
     
-    prompt = ChatPromptTemplate.from_template("AI Lab306, l'assistente AI del laboratorio di Interaction Design.\
-                                               Conosci le specializzazioni del nostro team:\
+    prompt = ChatPromptTemplate.from_template("AI Lab306, the AI assistant of the Interaction Design laboratory.\
+                                               You know the specializations of our team:\
                                                Antonio in Interaction Design,\
                                                Mario in NLP,\
-                                               Giulia in matematica e AI,\
+                                               Giulia in mathematics and AI,\
                                                Anna in UX Design,\
                                                Leonardo B. in web design,\
-                                               Leonardo G. in programmazione,\
+                                               Leonardo G. in programming,\
                                                Martina in marketing,\
-                                               e Claudia in analisi dei dati.\
-                                               Il tuo ruolo è offrire consigli basati su queste competenze,\
-                                               analizzare dati, facilitare la comunicazione e coordinare il team.\
-                                               Rispondi alle e-mail con un tono positivo e motivazionale,\
-                                                scrivi sempre in lingua italiana\
-                                               mantenendo un linguaggio chiaro e professionale. {topic}")
+                                               and Claudia in data analysis.\
+                                               Your role is to offer advice based on these skills,\
+                                               analyze data, facilitate communication and coordinate the team.\
+                                               Respond to emails with a positive and motivational tone,\
+                                                always write in Italian\
+                                               maintaining a clear and professional language. {topic}")
 
     # using LangChain Expressive Language chain syntax
     # learn more about the LCEL on
@@ -110,28 +98,28 @@ def process_email(body):
     llm_response = (chain.invoke({"topic": body }))
     return(llm_response)
 
-# Invia llm_response come risposta all'email
+# Send llm_response as a response to the email
 def send_email_response(llm_response, email_message):
     msg = MIMEText(llm_response)
-    msg['Subject'] = 'Risposta Automatica da AiLab306'
+    msg['Subject'] = 'Automatic Response from AiLab306'
     msg['From'] = email_account
     msg['To'] = email_message['From']
     
-    # Aggiungi destinatari in CC se presenti
+    # Add recipients in CC if present
     if 'Cc' in email_message:
         msg['Cc'] = email_message['Cc']
     
-    with smtplib.SMTP_SSL(smtp_host, 465) as smtp:  # Nota l'uso di SMTP_SSL e la porta 465
+    with smtplib.SMTP_SSL(smtp_host, 465) as smtp:  # Note the use of SMTP_SSL and port 465
         smtp.login(email_account, email_password)
-        # Includi sia i destinatari 'To' che 'Cc' quando invii il messaggio
+        # Include both 'To' and 'Cc' recipients when sending the message
         recipients = [email_message['From']]
         if 'Cc' in email_message:
             recipients += [email.strip() for email in email_message['Cc'].split(',') if email.strip() != email_account]
         smtp.send_message(msg, to_addrs=recipients)
 
-# Funzione per salvare la domanda e la risposta in un file di log excel e 
+# Function to save the question and answer in an excel log file and 
 def save_qa_to_log(question, answer):
-    data = {'Domanda': [question], 'Risposta': [answer]}
+    data = {'Question': [question], 'Answer': [answer]}
     df = pd.DataFrame(data)
     try:
         existing_df = pd.read_excel('ai_lab_logs.xlsx')
@@ -140,30 +128,32 @@ def save_qa_to_log(question, answer):
     except FileNotFoundError:
         df.to_excel('ai_lab_logs.xlsx', index=False)
     except Exception as e:
-        print("Errore durante il salvataggio nel file di log:", str(e))
+        print("Error while saving to log file:", str(e))
     else:
-        print("Salvataggio nel file di log completato con successo.")
+        print("Saving to log file completed successfully.")
     #save also in a text file
     with open('ai_lab_logs.txt', 'a') as f:
-        f.write(f'Domanda: {question}\nRisposta: {answer}\n\n')
+        f.write(f'Question: {question}\nAnswer: {answer}\n\n')
         #save_qa_to_vector_store(f)
 
 
 # main loop
 while True:
-        print("Attesa per il prossimo controllo...")
-        # Esegui il controllo delle email
-        email_message = check_for_new_emails(email_account)
-        # Estrai il corpo dell'email
+    print("Waiting for the next check...")
+    # Perform the email check
+    email_message = check_for_new_emails(email_account)
+    # If there is a new email, process it
+    if email_message is not None:
+        # Extract the body of the email
         body = get_email_body(email_message)
-        print("Corpo dell'email: ", body)
-        # Processa il corpo dell'email
+        print("Email body: ", body)
+        # Process the body of the email
         llm_response = process_email(body)
-        print("Risposta generata da LLM: ", llm_response)
-        # Invia la risposta all'email
+        print("Response generated by LLM: ", llm_response)
+        # Send the response to the email
         send_email_response(llm_response, email_message)
-        print("Risposta inviata con successo.")
-        # Salva la domanda e la risposta nel file di log
+        print("Response sent successfully.")
+        # Save the question and answer in the log file
         # save_qa_to_log(body, llm_response)
-        # print("Domanda e risposta salvate nel file di log.")
-        time.sleep(300)  # Pausa di 5 minuti
+        # print("Question and answer saved in the log file.")
+    time.sleep(300)  # Pause for 5 minutes
--- readme.md
+++ readme.md
@@ -1,8 +1,15 @@
 # AI Mail
 
 AI Mail is a Python application that uses AI to interact with emails.
-It replies to the mail sent to the configured email address. It utilizes Ollama, as AI framework.
+It replies to the mail sent to the configured email address. 
+
+It utilizes Ollama, if you want a local machine elaboration (GPU required).
+Or it can uses Groq, an API (free actually, with some limits) tha uses a remote Language Processing Unit and some Open Source LLMs, it has a very fast inference speed.
+
 To install Ollama, please refer to the official GitHub repository: [https://github.com/ollama/ollama](https://github.com/ollama/ollama)
+
+To use Groq, check: https://groq.com
+
 We use this into our university lab, so you will references of our team into the prompts, please modify as your needs
 
 ## Setup
@@ -17,9 +24,16 @@ git clone <repository-url>
 cd <project-directory>
 ```
 
-3. Create a `.env` file in the root of your project and add your email password:
+3. Create a `.env` file in the root of your project and add your data:
 ```env
-EMAIL_PASSWORD=your-app-password
+EMAIL_PASSWORD = your-app-password (i.e. for gmail needs to generate an app pw)
+EMAIL = the email that you want to automate
+
+IMAP = mail imap
+SMTP = mail smtp
+
+LANGCHAIN_API_KEY= your Langchain api key
+GROQ_API_KEY= your Groq api key
 ```
 Replace `'your-app-password'` with your actual app password.
 
@@ -39,11 +53,11 @@ To create an App Password for your Gmail account, follow these steps:
 
 ## Usage
 
-The main functionality of the application is contained in the `AI_mail.py` script. The script uses the `os` module and `python-dotenv` to securely access your email password, and the `ChatOllama` and `OllamaEmbeddings` classes for AI functionalities.
+The main functionality of the application is contained in the `AI_mail.py` script. The script uses the `os` module and `python-dotenv` to securely access your email password, and the `ChatOllama` and `OllamaEmbeddings` classes for AI functionalities, or the `ChatGroq` if you want to use the remote LPU
 
 The `check_for_new_emails` function is where the main logic of the application resides.
 
-Added Listen function, in deployment, to give the ability to transcribe audio to text and summarize it, usefull for sharing report of meetings
+In deployment, a `listen_base` function to give the ability to transcribe audio to text and summarize it, usefull for sharing report of meetings, also we would like to implement a RAG system based on the lab website, `generate_response_rag`.
 
 ## Contributing
 
